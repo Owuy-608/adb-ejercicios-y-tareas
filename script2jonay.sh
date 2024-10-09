0ee96722e36880e76bb5be425418680f47ae1010
owuy <alejandrobejaranogutierrez@gmail.com>
2024-10-09 14:44:06 +0100

    empezando con los ejercicios

---
+++ script2jonay.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Crear la estructura de directorios
+mkdir -p ut01-ejercicios/01-ficheros/ejercicio01
+mkdir -p ut01-ejercicios/01-ficheros/ejercicio02
+mkdir -p ut01-ejercicios/01-ficheros/ejercicio03
+mkdir -p ut01-ejercicios/01-ficheros/ejercicio04
+mkdir -p ut01-ejercicios/01-ficheros/ejercicio05
+mkdir -p ut01-ejercicios/02-bbdd/ejercicio01
+mkdir -p ut01-ejercicios/02-bbdd/ejercicio02
+mkdir -p ut01-ejercicios/03-sgbd/ejercicio01
+mkdir -p ut01-ejercicios/03-sgbd/ejercicio02
+mkdir -p ut01-ejercicios/03-sgbd/ejercicio03
+mkdir -p ut01-ejercicios/03-sgbd/ejercicio04
+
+# Crear los archivos en la estructura
+touch ut01-ejercicios/01-ficheros/ejercicio01/libros.csv
+touch ut01-ejercicios/01-ficheros/ejercicio02/canciones.csv
+touch ut01-ejercicios/01-ficheros/ejercicio03/direcciones.csv
+touch ut01-ejercicios/01-ficheros/ejercicio03/leer_csv.md
+touch ut01-ejercicios/01-ficheros/ejercicio03/leer_csv.py
+touch ut01-ejercicios/01-ficheros/ejercicio04/direcciones.csv
+touch ut01-ejercicios/01-ficheros/ejercicio04/leer_csv.md
+touch ut01-ejercicios/01-ficheros/ejercicio04/leer_csv.py
+touch ut01-ejercicios/01-ficheros/ejercicio05/leer_csv.md
+touch ut01-ejercicios/01-ficheros/ejercicio05/leer_csv.py
+touch ut01-ejercicios/01-ficheros/ejercicio05/productos.csv
+touch ut01-ejercicios/02-bbdd/ejercicio01/ventajas-inconvenientes-bbdd-relacionales.md
+touch ut01-ejercicios/02-bbdd/ejercicio02/ventajas-inconvenientes-bbdd-nosql.md
+touch ut01-ejercicios/03-sgbd/ejercicio01/escalabilidad.md
+touch ut01-ejercicios/03-sgbd/ejercicio02/wordpress.md
+touch ut01-ejercicios/03-sgbd/ejercicio03/odoo.md
+touch ut01-ejercicios/03-sgbd/ejercicio04/empresas.md
+
+echo "Estructura de directorios y archivos creada exitosamente."
